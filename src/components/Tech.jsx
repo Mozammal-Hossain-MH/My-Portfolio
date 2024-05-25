64325336681379c6ce48eefb4ad5c805d9ecb904
Mozammal <mozammalhossain33@gmail.com>
2024-05-26 00:58:24 +0600

    experience and tech section added

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import { technologies } from "../constants";
+import Ball from "./canvas/Ball";
+import { SectionWrapper } from "../hoc";
 
 const Tech = () => {
   return (
-    <div>Tech</div>
-  )
-}
+    <div className={`flex flex-row flex-wrap justify-center gap-10`}>
+      {technologies?.map((tech) => (
+        <div key={tech.name} className={`h-28 w-28`}>
+          <Ball icon={tech?.icon} />
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default Tech
+export default SectionWrapper(Tech, "");
