53b36f3c283efadab2c041cc8b8c7963a5eb7cd8
Mozammal <mozammalhossain33@gmail.com>
2024-05-31 00:31:26 +0600

    UI complete

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,90 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
+import { motion } from "framer-motion";
+import { SectionWrapper } from "../hoc";
+import { slideIn } from "../utils/motion";
+import { styles } from "../styles";
+import Earth from "./canvas/Earth";
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+  const formRef = useRef();
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [loading, setLoading] = useState(false);
 
-export default Contact
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div
+      className={`xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden`}
+    >
+      <motion.div
+        variants={slideIn("left", "tween", 0.2, 1)}
+        className={`flex-[0.75] bg-black-100 p-8 rounded-2xl`}
+      >
+        <p className={`${styles.sectionSubText}`}>Get in touch</p>
+        <p className={`${styles.sectionHeadText}`}>Contact.</p>
+        <form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className={`mt-12 flex flex-col gap-8`}
+        >
+          <label className={`flex flex-col`}>
+            <span className={`text-white font-medium mb-4`}> Your Name</span>
+            <input
+              type="text"
+              name="name"
+              value={form.name}
+              onChange={handleChange}
+              placeholder="Your name"
+              className={`bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium`}
+            />
+          </label>
+          <label className={`flex flex-col`}>
+            <span className={`text-white font-medium mb-4`}> Your Email</span>
+            <input
+              type="email"
+              name="email"
+              value={form.email}
+              onChange={handleChange}
+              placeholder="Your email"
+              className={`bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium`}
+            />
+          </label>
+          <label className={`flex flex-col`}>
+            <span className={`text-white font-medium mb-4`}> Your Message</span>
+            <textarea
+              rows={7}
+              name="message"
+              value={form.message}
+              onChange={handleChange}
+              placeholder="Please enter your message"
+              className={`bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium`}
+            />
+          </label>
+          <button
+            className={`bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary `}
+          >
+            {loading ? "Sending..." : "Send"}
+          </button>
+        </form>
+      </motion.div>
+
+      <motion.div
+        variants={slideIn("right", "tween", 0.2, 1)}
+        className={`xl:flex-1 xl:h-auto md:h-[550px] h-[350px]`}
+      >
+        <Earth />
+      </motion.div>
+    </div>
+  );
+};
+
+export default SectionWrapper(Contact, "contact");
--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import { Canvas } from "@react-three/fiber";
+import React, { Suspense } from "react";
+import Loader from "../Loader";
+import { OrbitControls, useGLTF } from "@react-three/drei";
+
+const EarthModel = () => {
+  const earth = useGLTF("./planet/scene.gltf");
+  return (
+    <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
+  );
+};
 
 const Earth = () => {
   return (
-    <div>Earth</div>
-  )
-}
+    <Canvas
+      shadows
+      frameloop="demand"
+      gl={{ preserveDrawingBuffer: true }}
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [-4, 3, 6],
+      }}
+    >
+      <Suspense fallback={<Loader />}>
+        <OrbitControls
+          autoRotate
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <EarthModel />
+      </Suspense>
+    </Canvas>
+  );
+};
 
-export default Earth
+export default Earth;
